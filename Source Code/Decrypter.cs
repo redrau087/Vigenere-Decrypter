e399b31c64f56b67b76a8d17c621a08b108a840e
Travis Rau <40340268+redrau087@users.noreply.github.com>
2022-10-28 02:47:55 -0400

    Modified algorithms and added explaining scripts

--- Source Code/Decrypter.cs
+++ Source Code/Decrypter.cs
@@ -32,6 +32,22 @@ namespace VigenereDecryptionTools
 
         #region Static Methods
         /// <summary>
+        /// Performs a vigenere encryption with the given plain text and key
+        /// </summary>
+        /// <param name="plaintext">The plain text to be encrypted</param>
+        /// <param name="key">Then encryption key</param>
+        /// <returns></returns>
+        public static string Encrypt(string plaintext, string key)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int x = 0; x < plaintext.Length; x++)
+                sb.Append((char)(((plaintext[x] - 'A' + key[x % key.Length] + 1) % 26) + 'A'));
+
+            return sb.ToString();
+        }
+
+        /// <summary>
         /// Iterates through each character in the provided cipher text and decrypts it using the given key
         /// </summary>
         /// <param name="cipherTextIn">The cipher text to decrypt</param>
@@ -82,7 +98,7 @@ namespace VigenereDecryptionTools
         /// Performs a frequency analysis testing multiple different key lengths to determine the most probable key length
         /// </summary>
         /// <returns>The key length or 0 if not found</returns>
-        public ushort FindKeyLength()
+        public byte FindKeyLength()
         {
             StringAnalyzer analyzer = new StringAnalyzer();
             string cipherText;
@@ -94,8 +110,8 @@ namespace VigenereDecryptionTools
                 //we only need to check one of the new strings for frequency analysis so we can select only the first
                 analyzer.Analyze(cipherText);
 
-                if (analyzer.HighestProbability > 10 && analyzer.LowestProbability < 1)
-                    //one letter should be ~13% and multiple should be less than 1% so we can return if this is found
+                if (analyzer.MeanAbsoluteDeviation > 2.7)
+                    //incorrect key lengths will have a much lower MAD
                     return x;
             }
 
@@ -103,21 +119,39 @@ namespace VigenereDecryptionTools
         }
 
         /// <summary>
-        /// Finds the key by determining the distance between 'E' and the most frequent letter analyzed to discover each character of the key at a time
+        /// Finds the key by determining the closest alignment between the standard frequency and the decrypted ciphertext
         /// </summary>
         /// <param name="keyLength"></param>
         /// <returns>Each character of the key as it's discovered</returns>
-        private IEnumerable<char> FindKey(ushort keyLength)
+        private IEnumerable<char> FindKey(byte keyLength)
         {
             StringAnalyzer analyzer = new StringAnalyzer();
+            double currentScore;
+            double lowestScore;
+            byte lowestScoreIndex;
 
             foreach (string cipher in SeparateStrings(keyLength))
             {
                 analyzer.Clear();
 
                 analyzer.Analyze(DecryptSingleKey(cipher, 'A'));
+                lowestScore = analyzer.DeviationFromStandardFrequency;
+                lowestScoreIndex = 0;
 
-                yield return (char)(analyzer.HighestProbabilityCharacter - 'E' + 'A');
+                for (byte x = 1; x < 26; x++)
+                {
+                    analyzer.Clear();
+                    analyzer.Analyze(DecryptSingleKey(cipher, (char)('A' + x)));
+                    currentScore = analyzer.DeviationFromStandardFrequency;
+
+                    if (currentScore < lowestScore)
+                    {
+                        lowestScore = currentScore;
+                        lowestScoreIndex = x;
+                    }
+                }
+
+                yield return (char)('A' + lowestScoreIndex);
             }
         }
 
@@ -146,6 +180,117 @@ namespace VigenereDecryptionTools
         /// </summary>
         /// <returns>The decrypted plaintext</returns>
         public string Decrypt() => Decrypt(FindEncryptionKey());
+        #endregion
+
+        #region Explaining
+        /// <summary>
+        /// Decrypts the ciphertext and explains the steps to finding the key while solving
+        /// </summary>
+        public void DecryptAndExplain()
+        {
+            //To decrypt the ciphertext we must find the key but to find the key the first step is to find the length of the key
+            //Incorrect key lengths will have irregular letter frequencies. We can exploit this by testing different key lengths until we find a normal-looking distribution
+            StringAnalyzer analyzer = new StringAnalyzer();
+
+            analyzer.Clear(); //clear analysis from previous test
+
+            cipherText = SeparateStrings(5).ToArray()[0];
+            //we only need to check one of the new strings for frequency analysis so we can select only the first
+            analyzer.Analyze(cipherText);
+
+            Console.WriteLine(analyzer.FormattedLetterFrequencies);
+            Console.WriteLine($"\nMAD: {analyzer.MeanAbsoluteDeviation}");
+        }
+
+        /// <summary>
+        /// Finds the length of the key used to encrypt the cipher text and explains the process
+        /// </summary>
+        /// <returns>The key length</returns>
+        public byte FindKeyLengthAndExplain()
+        {
+            StringAnalyzer analyzer = new StringAnalyzer();
+            string cipherText;
+            for (byte x = 1; x < byte.MaxValue; x++)
+            {
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey(); //hold so you can read previous data
+                Console.Clear(); //clear the console
+
+
+                analyzer.Clear(); //clear analysis from previous test
+
+                cipherText = SeparateStrings(x).ToArray()[0];
+                //we only need to check one of the new strings for frequency analysis so we can select only the first
+                analyzer.Analyze(cipherText);
+
+                double mad = analyzer.MeanAbsoluteDeviation;
+
+                if (mad > 2.7)
+                {
+                    Console.WriteLine($"A mean absolute deviation of {mad} is high enough so the expected key length is {x}");
+                    //incorrect key lengths will have a much lower MAD
+                    return x;
+                }
+                else
+                    Console.WriteLine($"A mean absolute deviation of {mad} is too low so a key length of {x} is unlikely");
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Finds the encryption key used to encrypt the cipher text and explains the process
+        /// </summary>
+        /// <param name="keyLength">The length of the key to find</param>
+        /// <returns>The encryption key</returns>
+        public string FindEncryptionKeyAndExplain(byte keyLength)
+        {
+            StringAnalyzer analyzer = new StringAnalyzer();
+
+            StringBuilder keyFound = new StringBuilder();
+
+            double currentScore;
+            double lowestScore;
+            byte lowestScoreIndex;
+
+            foreach (string cipher in SeparateStrings(keyLength))
+            {
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey(); //hold so you can read previous data
+                Console.Clear(); //clear the console
+                Console.WriteLine($"Current key discovered: {keyFound.ToString()}");
+                analyzer.Clear();
+
+
+
+                analyzer.Analyze(DecryptSingleKey(cipher, 'A'));
+                lowestScore = analyzer.DeviationFromStandardFrequency;
+                lowestScoreIndex = 0;
+
+                for (byte x = 1; x < 26; x++)
+                {
+                    analyzer.Clear();
+                    analyzer.Analyze(DecryptSingleKey(cipher, (char)('A' + x)));
+                    currentScore = analyzer.DeviationFromStandardFrequency;
+
+                    if (currentScore < lowestScore)
+                    {
+                        lowestScore = currentScore;
+                        lowestScoreIndex = x;
+                    }
+                }
+
+                Console.WriteLine($"Key found: {(char)(lowestScoreIndex + 'A')} with a deviation of {lowestScore}" +
+                    $"\nFrequency analysis using this key: ");
+                analyzer.Clear();
+                analyzer.Analyze(DecryptSingleKey(cipher, (char)(lowestScoreIndex + 'A')));
+                Console.WriteLine(analyzer.FormattedLetterFrequencies);
+
+                keyFound.Append((char)(lowestScoreIndex + 'A'));
+            }
+
+            return keyFound.ToString();
+        }
         #endregion
     }
 }
